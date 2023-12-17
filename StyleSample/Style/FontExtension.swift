d572541f9806ec678385efb59d74b310fbbf3573
リョウ <121738322+RyoDeveloper@users.noreply.github.com>
2023-12-18 00:52:54 +0900

    文言の修正

--- StyleSample/Style/FontExtension.swift
+++ StyleSample/Style/FontExtension.swift
@@ -10,22 +10,22 @@ import Foundation
 import SwiftUI
 
 extension Font {
-    /// システムフォント かつ ダイナミックタイプに対応させたい時
+    /// システムフォントで、ダイナミックタイプに対応させる
     static var systemDynamic: Font {
         return Font.custom("", size: 20)
     }
 
-    /// システムフォント かつ ダイナミックタイプに対応させたくない時
+    /// システムフォントで、ダイナミックタイプに対応させない
     static var systemFixed: Font {
         return Font.system(size: 20)
     }
 
-    /// カスタムフォント かつ ダイナミックタイプに対応させたい時
+    /// カスタムフォントで、ダイナミックタイプに対応させる
     static var customDynamic: Font {
         return Font.custom("FontName", size: 20)
     }
 
-    /// カスタムフォント かつ ダイナミックタイプに対応させたくない時
+    /// カスタムフォントで、 ダイナミックタイプに対応させない
     static var customFixed: Font {
         return Font.custom("FontName", fixedSize: 20)
     }
